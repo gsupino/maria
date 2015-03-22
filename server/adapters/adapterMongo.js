ea32322c1025553dcc5a3a021ce66c962637b624
Gianluca Supino <gianluca.supino@gmail.com>
2015-03-22 12:59:09 +0100

    add getOne

--- server/adapters/adapterMongo.js
+++ server/adapters/adapterMongo.js
@@ -5,40 +5,48 @@ import config from'../config/environment';
 
 class AdapterMongo {
 
-  constructor(){
-    console.log('mongo');
-    this.db=monk(config.mongo.uri,config.mongo.options);
-  }
+    constructor() {
+        console.log('mongo');
+        this.db = monk(config.mongo.uri, config.mongo.options);
+    }
 
-  *getQuery(collection,id,query,options){
-    let q=query ||{}
-    let opt=options || {}
-    let col=wrap(this.db.get(collection))
-    try{
-      let result = yield (col.find(q,opt));
-      return result;
+    * getQuery(collection, query, options) {
+        let q = query || {}
+        let opt = options || {}
+        let col = wrap(this.db.get(collection))
+        try {
+            let result = yield(col.find(q, opt));
+            return result;
+        } catch (e) {
+            return e;
+        }
     }
-    catch(e){
-      return e;
-    }
-  }
 
-  *getById(collection,id){
-    let col=wrap(this.db.get(collection))
-    try{
-      let result = yield (col.findById(id));
-      return result;
+    * getById(collection, id) {
+        let col = wrap(this.db.get(collection))
+        try {
+            let result = yield(col.findById(id));
+            return result;
+        } catch (e) {
+            return e;
+        }
     }
-    catch(e){
-      return e;
-    }
-  }
 
-  close(){
-    this.db.close()
-   }
+    * getOne(collection, query, options) {
+        let q = query || {}
+        let opt = options || {}
+        let col = wrap(this.db.get(collection))
+        try {
+            let result = yield(col.findOne(q, opt));
+            return result;
+        } catch (e) {
+            return e;
+        }
+    }
+
+    close() {
+        this.db.close()
+    }
 }
 
 export let adapterMongo = new AdapterMongo();
-
-
